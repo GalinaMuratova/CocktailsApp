f75bf119d5e583111426c0490fb8695e01f24bfb
admins <ufkz200147@gmail.com>
2023-10-20 17:21:31 +0600

    Дописала запросы (delete, patch) для коктейлей, написала фикстуры

--- backend-cocktails/fixtures.ts
+++ backend-cocktails/fixtures.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import config from "./config";
 import crypto from "crypto";
 import User from "./models/User";
+import Cocktail from "./models/Cocktail";
 
 const run = async () => {
     await mongoose.connect(config.db);
@@ -9,10 +10,11 @@ const run = async () => {
 
     try {
         await db.dropCollection('users');
+        await db.dropCollection('cocktails')
     } catch (e) {
         console.log('Collection were not present');
     }
-    await User.create(
+    const [user1, user2] = await User.create(
         {
             email: 'anna@gmail.com',
             displayName: 'Anna Gavalda',
@@ -30,6 +32,107 @@ const run = async () => {
             avatar:'sam.jpg'
         },
     );
+
+    await Cocktail.create(
+        {
+            name: 'Bloody Mary',
+            recipe: 'Combine vodka and tomato juice in a mixing glass. Add salt, black pepper, cayenne pepper, lemon juice, and Worcestershire sauce. Add ice and stir well. Strain the mixture into a highball glass over ice. Garnish with a celery stalk and lemon wedge.',
+            ingredients: [
+                {
+                    ingredientName: 'Vodka',
+                    quantity: '2 ounces'
+                },
+                {
+                    ingredientName: 'Tomato juice',
+                    quantity: '4 ounces'
+                },
+            ],
+            image: 'bloody-mary.jpg',
+            user: user1._id,
+            published: true
+        },
+        {
+            name: 'Margarita',
+            recipe: 'Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.',
+            ingredients: [
+                {
+                    ingredientName: 'Tequila',
+                    quantity: '1/2 ounces'
+                },
+                {
+                    ingredientName: 'Lime juice',
+                    quantity: '1 ounce'
+                },
+                {
+                    ingredientName: 'Salt',
+                    quantity: '1 tablespoon'
+                }
+            ],
+            image: 'Margarita.jpg',
+            user: user1._id,
+            published: false
+        },
+        {
+            name: 'Daiquiri',
+            recipe: 'Pour all ingredients into shaker with ice cubes. Shake well. Strain in chilled cocktail glass.',
+            ingredients: [
+                {
+                    ingredientName: 'White rum',
+                    quantity: '1 1/2 ounces'
+                },
+                {
+                    ingredientName: 'Lime juice',
+                    quantity: '1/2 ounce'
+                },
+                {
+                    ingredientName: 'Simple syrup',
+                    quantity: '1 teaspoon'
+                }
+            ],
+            image: 'Daiquiri.jpg',
+            user: user1._id,
+            published: true
+        },
+        {
+            name: 'Martini',
+            recipe: 'Stir the gin and vermouth in a mixing glass with ice. Strain into a chilled cocktail glass. Garnish with an olive or a twist of lemon.',
+            ingredients: [
+                {
+                    ingredientName: 'Gin',
+                    quantity: '1 1/2 ounces'
+                },
+                {
+                    ingredientName: 'Dry vermouth',
+                    quantity: '1/2 ounce'
+                }
+            ],
+            image: 'martini.jpg',
+            user: user2._id,
+            published: false
+        },
+        {
+            name: 'Mojito',
+            recipe: 'Muddle the mint leaves with simple syrup and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish with sprig of mint leaves and lemon slice.',
+            ingredients: [
+                {
+                    ingredientName: 'White rum',
+                    quantity: '2 ounces'
+                },
+                {
+                    ingredientName: 'Lime juice',
+                    quantity: '1 ounce'
+                },
+                {
+                    ingredientName: 'Simple syrup',
+                    quantity: '2 teaspoons'
+                }
+            ],
+            image: 'mojito.webp',
+            user: user2._id,
+            published: true,
+        }
+    );
+
     await db.close();
 };
 run().catch(console.error);
--- backend-cocktails/routers/cocktails.ts
+++ backend-cocktails/routers/cocktails.ts
@@ -2,11 +2,12 @@ import express from "express";
 import Cocktail from '../models/Cocktail';
 import auth, {RequestWithUser} from "../middleware/auth";
 import {imagesUpload} from "../multer";
+import permit from "../middleware/permit";
 
 
 const cocktailsRouter =express.Router();
 
-cocktailsRouter.get('/', async (req, res, next) => {
+cocktailsRouter.get('/', auth, permit('admin'), async (req, res) => {
     try {
         const cocktails = await Cocktail.find();
         return res.send(cocktails);
@@ -15,9 +16,47 @@ cocktailsRouter.get('/', async (req, res, next) => {
     }
 });
 
-cocktailsRouter.post('/', auth, imagesUpload.single('image'), async (req,  res, next) => {
+cocktailsRouter.get('/published', async (req, res) => {
+    try {
+        const cocktails = await Cocktail.find({ published: true })
+            .sort({ name: 1 });
+        return res.send(cocktails);
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(500);
+    }
+});
+
+cocktailsRouter.get('/user', auth, async (req, res) => {
     const user = (req as RequestWithUser).user;
-    console.log(user);
+    try {
+        if (!user._id) {
+            return res.status(400).send({error: 'Invalid user ID'});
+        }
+
+        const cocktails = await Cocktail.find({user: user._id});
+
+        return res.send(cocktails);
+    } catch (error) {
+        console.error(error);
+        return
+    }
+});
+
+cocktailsRouter.get('/:id', async (req, res) => {
+    try {
+        const cocktail = await Cocktail.findById(req.params.id);
+        if (!cocktail) {
+            return  res.sendStatus(404);
+        }
+        return res.send(cocktail);
+    } catch {
+        return res.sendStatus(500);
+    }
+});
+
+cocktailsRouter.post('/', auth, imagesUpload.single('image'), async (req,  res) => {
+    const user = (req as RequestWithUser).user;
     try {
        const cocktail = new Cocktail({
            user: user._id,
@@ -35,4 +74,36 @@ cocktailsRouter.post('/', auth, imagesUpload.single('image'), async (req,  res, 
     }
 });
 
+cocktailsRouter.patch('/:id/togglePublished', auth, permit('admin'), async (req, res, next) => {
+    try {
+        const publishedCocktail = await Cocktail.findById(req.params.id);
+
+        if (!publishedCocktail) {
+            return res.sendStatus(404);
+        }
+
+        publishedCocktail.published = !publishedCocktail.published;
+
+        await publishedCocktail.save();
+
+        return res.send({ published: publishedCocktail.published });
+    } catch (e) {
+        next(e);
+    }
+});
+
+cocktailsRouter.delete('/:id', auth, permit('admin'), async (req, res, next) => {
+    try {
+        const album = await Cocktail.findOne({ _id: req.params.id });
+
+        if (!album) {
+            return res.sendStatus(403);
+        }
+        await Cocktail.deleteOne({ _id: req.params.id });
+
+        return res.sendStatus(204);
+    } catch (e) {
+        next(e);
+    }
+});
 export default cocktailsRouter;
